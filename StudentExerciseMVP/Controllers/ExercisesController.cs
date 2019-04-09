959e2e89b6244f4bab921f99a6f4413870267996
Joseph <jbaugh78@msn.com>
2019-04-09 09:16:59 -0500

    exercise deletes

--- StudentExerciseMVP/Controllers/ExercisesController.cs
+++ StudentExerciseMVP/Controllers/ExercisesController.cs
@@ -146,17 +146,39 @@ namespace StudentExerciseMVP.Controllers
         // GET: Exercises/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Exercise exercise = GetExerciseById(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+
+            ExerciseDeleteViewModel viewModel = new ExerciseDeleteViewModel
+            {
+                Exercise = exercise
+            };
+
+            return View(viewModel);
         }
 
         // POST: Exercises/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, ExerciseDeleteViewModel viewModel)
         {
             try
             {
-                // TODO: Add delete logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM exercise WHERE id = @id;";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
 
                 return RedirectToAction(nameof(Index));
             }
@@ -164,6 +186,36 @@ namespace StudentExerciseMVP.Controllers
             {
                 return View();
             }
+
         }
+            private Exercise GetExerciseById(int id)
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"SELECT Id, ExerciseName, ExerciseLanguage
+                                            FROM Exercise
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Exercise exercise = null;
+
+                        if (reader.Read())
+                        {
+                            exercise = new Exercise
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                                ExerciseLanguage = reader.GetString(reader.GetOrdinal("ExerciseLanguage"))
+                            };
+                        }
+                        reader.Close();
+                        return exercise;
+                    }
+                }
+            }
     }
 }
