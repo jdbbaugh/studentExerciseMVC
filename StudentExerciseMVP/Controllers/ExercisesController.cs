eef8b06b9126e1cb01cac1b3bb0b6d05f12eeb63
Joseph <jbaugh78@msn.com>
2019-04-09 04:30:17 -0500

    exercise get now listing to browser

---
+++ StudentExerciseMVP/Controllers/ExercisesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExerciseMVP.Models;
+
+namespace StudentExerciseMVP.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ExercisesController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            }
+
+        }
+        // GET: Exercises
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, exerciseName, exerciseLanguage FROM Exercise";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Exercise> exercises = new List<Exercise>();
+
+                    while (reader.Read())
+                    {
+                        Exercise exercise = new Exercise
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("id")),
+                            ExerciseName = reader.GetString(reader.GetOrdinal("exerciseName")),
+                            ExerciseLanguage = reader.GetString(reader.GetOrdinal("exerciseLanguage"))
+                        };
+                        exercises.Add(exercise);
+                    }
+                    reader.Close();
+                    return View(exercises);
+                }
+            }
+        }
+
+        // GET: Exercises/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Exercises/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exercises/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exercises/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Exercises/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Exercises/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Exercises/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ StudentExerciseMVP/Models/Exercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExerciseMVP.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Exercise Name")]
+        public string ExerciseName { get; set; }
+
+        [Required]
+        [Display(Name = "Language")]
+        public string ExerciseLanguage { get; set; }
+    }
+}
